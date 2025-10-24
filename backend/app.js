b7052ecec91f6dfdc6be62f2f80d025e86f6d701
Aidesu <waoxsan@gmail.com>
2025-10-24 12:26:18 +0200

    Setting up package

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,10 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+
+const app = express();
+const port = process.env.PORT;
+
+connectDB();
+app.use(express.json());
+
+app.listen(port, () => console.log("Server start on port : " + port));
