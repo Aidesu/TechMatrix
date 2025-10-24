b11cdd5552039803037233180d450dd33aee1684
brian <brian.pro1993@gmail.com>
2025-10-24 16:54:58 +0200

    get,post,put in user

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import usersRoute from "./routes/usersRoute.js";
 
 const app = express();
 const port = process.env.PORT;
 
-connectDB();
+await connectDB();
 app.use(express.json());
+
+app.use("/users", usersRoute);
 
 app.listen(port, () => console.log("Server start on port : " + port));
--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 connectDB().catch((err) => console.log(err));
 
 export async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+	} catch (e) {
+		res.status(500).json({ message: e.message });
+	}
 }
