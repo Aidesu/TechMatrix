7dcf4c24606278b5ab99b8f06f4d93fde25e609a
Brian Farnier <brian.pro1993@gmail.com>
2025-10-25 15:35:59 +0200

    regexPassword

--- backend/models/users.js
+++ backend/models/users.js
@@ -24,9 +24,16 @@ export async function addUser(newUser) {
 		return false;
 	}
 
+
+	const allRegxPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+
+	if (!allRegxPassword.test(newUser.password)) {
+		throw new Error("Password must contain one uppercase , lowercase, one number, and least 8 characters long ");
+	}
+
+
 	const saltRounds = 10;
 	const password_hash = await bcrypt.hash(newUser.password, saltRounds);
-
 
 	const user = new User({
 		...newUser,
