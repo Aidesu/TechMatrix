128e548dc80b88bfc8ed57fc0a5fd5bfc3a3c478
Brian Farnier <brian.pro1993@gmail.com>
2025-10-27 16:17:06 +0100

    add account + CSS reponsive

--- frontend/TechMatrix-Front/src/views/account/Account.jsx
+++ frontend/TechMatrix-Front/src/views/account/Account.jsx
@@ -1,22 +1,43 @@
 import { useEffect, useState } from "react"
+import { getUserById } from "../../api/api";
 
 
 export default function Account() {
 
     const [user, setUser] = useState(null);
+    const getLocalUser = localStorage.getItem("user");
 
     useEffect(() => {
-        const getLocalUser = localStorage.getItem("user");
-        if (getLocalUser) {
-            setUser(JSON.parse(getLocalUser));
+        async function fetchData() {
+            const userData = await getUserById(getLocalUser);
+            setUser(userData);
         }
-    }, []);
+        fetchData();
+    }, [getLocalUser]);
+
+
 
     return (
         <main>
             <h1>My account</h1>
+            <div class="account-container">
+                <div class="account-image">
+                    <img src={user?.image} />
+                </div>
+                <div class="account-info">
+                    <p>Username: {user?.username}</p>
+                    <p>First name: {user?.name.first_name}</p>
+                    <p>Last name: {user?.name.last_name}</p>
+                    <p>Email address: {user?.email}</p>
+                    <p>joined at: {user?.joined_at}</p>
 
-            <p>Username: {user?.username}</p>
+                    <div class="account-buttons">
+                        <button>Edit</button>
+                        <button class="btn-disconect">Disconect</button>
+                    </div>
+                </div>
+
+            </div>
 
         </main>
     )
