6925df2bf6a5a35af0ee2062cb1c9ae780a456b4
Aidesu <waoxsan@gmail.com>
2025-10-27 16:28:14 +0100

    Update CRUD gpu u and d

--- frontend/TechMatrix-Front/src/views/account/Account.jsx
+++ frontend/TechMatrix-Front/src/views/account/Account.jsx
@@ -1,23 +1,25 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import { getUserById } from "../../api/api";
 
 export default function Account() {
-
     const [user, setUser] = useState(null);
+    const getLocalUser = localStorage.getItem("user");
+    if (!getLocalUser) return;
 
     useEffect(() => {
-        const getLocalUser = localStorage.getItem("user");
-        if (getLocalUser) {
-            setUser(JSON.parse(getLocalUser));
+        async function fetchData() {
+            const userData = await getUserById(getLocalUser);
+            setUser(userData);
+            console.log(user);
         }
-    }, []);
+        fetchData();
+    }, [getLocalUser]);
 
     return (
         <main>
             <h1>My account</h1>
 
             <p>Username: {user?.username}</p>
-
         </main>
-    )
+    );
 }
